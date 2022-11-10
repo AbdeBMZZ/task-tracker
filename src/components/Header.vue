e6b96f705f1ba7a4b290096313c653e000c7b60c
AbdeBMZZ <abdellahboumaiza1@gmail.com>
2022-11-10 11:05:18 +0100

    toggle reminder

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>Task Tracker</h1>
+    <Button text="Abdellah" color="green">Add</Button>
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div :key="task.id" v-for="task in tasks">
+      <Task
+        :task="task"
+        @delete-task="$emit('delete-task', task.id)"
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
+
+<style scoped></style>
